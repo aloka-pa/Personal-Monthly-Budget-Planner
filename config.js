fc2f7c994509b5a6498b5c7f6efa39288dbfffba
Aloka Pathiraja <w2052008@westminster.ac.uk>
2026-07-29 16:24:10 +0530

    keep supabase configs for github pages

---
+++ config.js
@@ -0,0 +1,6 @@
+// ============================================================
+// Supabase configuration
+// ------------------------------------------------------------
+
+const SUPABASE_URL = "https://ffzxrclztxqinufaedtx.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_JgjS0KjD9L-XLMvoOn-xSA__tmPfy4I";
