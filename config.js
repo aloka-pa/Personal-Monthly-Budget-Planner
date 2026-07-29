798d20ed54f1d4e7cc810a8d2015508fb0d0a175
Aloka Pathiraja <w2052008@westminster.ac.uk>
2026-07-29 16:19:44 +0530

    stop tracking config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// ============================================================
-// Supabase configuration
-// ------------------------------------------------------------
-
-const SUPABASE_URL = "https://ffzxrclztxqinufaedtx.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_JgjS0KjD9L-XLMvoOn-xSA__tmPfy4I";
